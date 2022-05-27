9700fc96eed0a7718307697cc4a466ba64414e10
Rasy <marasy@labs.haystack.asia>
2022-05-27 18:10:50 +0700

    Reschedule Alarm

--- lib/common/alarm_scheduler.dart
+++ lib/common/alarm_scheduler.dart
@@ -1,7 +1,10 @@
 import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:beerwarden/controllers/event_controller.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AlarmScheduler {
+  static const ALARM_KEY = "IS_ALARM_SET";
   static final AlarmScheduler _scheduler = AlarmScheduler._internal();
 
   AlarmScheduler._internal();
@@ -13,17 +16,16 @@ class AlarmScheduler {
   }
 
   void schedule() async {
+    var instance = await SharedPreferences.getInstance();
+    if (!(instance.getBool(ALARM_KEY) ?? false)) {
+      return;
+    }
     try {
       await AndroidAlarmManager.periodic(const Duration(days: 1), 1, onAlarm(),
           startAt: DateTime(
-              DateTime
-                  .now()
-                  .year, DateTime
-              .now()
-              .month, DateTime
-              .now()
-              .day, 1),
+              DateTime.now().year, DateTime.now().month, DateTime.now().day, 1),
           rescheduleOnReboot: true);
+      instance.setBool(ALARM_KEY, true);
     } catch (e) {
       print(e);
     }
@@ -38,5 +40,4 @@ class AlarmScheduler {
       print(e);
     }
   }
-
 }
